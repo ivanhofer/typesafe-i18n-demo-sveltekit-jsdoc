8802076f2456ba9cd69461e2fa38ce7c79c34605
Ivan Hofer <ivan.hofer@outlook.com>
2021-11-09 04:34:30 +0100

    setup i18n aliases

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,19 @@
+import path from 'path'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		
+		vite: {
+			resolve: {
+				alias: {
+					$i18n: path.resolve('./src/i18n'),
+					$lib: path.resolve('./src/lib'),
+				},
+			},
+		},
 	},
 }
 

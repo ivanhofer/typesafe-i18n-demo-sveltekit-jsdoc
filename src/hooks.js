e752aef843d8050c16f1ee75500bc96a99cdd8a6
Ivan Hofer <ivan.hofer@outlook.com>
2021-11-09 05:10:26 +0100

    correctly set html lang attribute

--- src/hooks.js
+++ src/hooks.js
@@ -1,6 +1,21 @@
 import { detectLocale } from '$i18n/i18n-util'
 import { initAcceptLanguageHeaderDetector } from 'typesafe-i18n/detectors'
 
+/** @type { import('@sveltejs/kit').Handle } */
+export const handle = async ({ request, resolve }) => {
+	const response = await resolve(request)
+
+	// read language slug
+	const [, lang] = request.path.split('/')
+
+	return {
+		...response,
+		// replace html lang attribute with correct language
+		body: (response.body || '').toString().replace('<html lang="en">', `<html lang="${lang}">`),
+	}
+}
+
+
 /** @type { import('@sveltejs/kit').GetSession } */
 export const getSession = (request) => {
 	// detect the preferred language the user has configured in his browser

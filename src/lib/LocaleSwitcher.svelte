b6aae032e087c20b7d563b154d8cee382e176095
Ivan Hofer <ivan.hofer@outlook.com>
2021-11-09 05:04:26 +0100

    add lang slug to store to locales state between pages

--- src/lib/LocaleSwitcher.svelte
+++ src/lib/LocaleSwitcher.svelte
@@ -1,16 +1,30 @@
 <script>
+	import { page } from '$app/stores'
 	import { setLocale, locale } from '$i18n/i18n-svelte'
 	import { locales } from '$i18n/i18n-util'
+	import { replaceLocaleInUrl } from '../utils'
 
 	/**
-    * @param { import('$i18n/i18n-types').Locales } locale 
-    * @return { Promise<void> }
-    */
+	 * @param { import('$i18n/i18n-types').Locales } locale
+	 * @return { Promise<void> }
+	 */
 	const switchLocale = async (locale) => {
 		// update store => loads new dictionary from server
 		await setLocale(locale)
+
+		// update url to reflect locale changes
+		history.pushState({ locale }, '', replaceLocaleInUrl(location.pathname, locale))
 	}
+	
+	// update locale when navigating via browser back/forward buttons
+	/** @param { PopStateEvent } event */
+	const handlePopStateEvent = async (event) => event.state.locale && (await setLocale(event.state.locale))
+
+	// update locale when page store changes
+	$: setLocale(/** @type { import('$i18n/i18n-types').Locales } page.params.lang */ ($page.params.lang))
 </script>
+
+<svelte:window on:popstate={handlePopStateEvent} />
 
 <ul>
 	{#each locales as l}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,16 @@
 	import { initI18n } from '$i18n/i18n-svelte'
 
 	/** @type { import('@sveltejs/kit').Load } */
-	export const load = async ({ session }) => {
+	export const load = async ({ page, session }) => {
+
+		// redirect to preferred language if user comes from page root
+		if (!page.params.lang) {
+			return {
+				status: 302,
+				redirect: `/${session.locale}`,
+			}
+		}
+
 		await initI18n(session.locale)
 
 		return {}

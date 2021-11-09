536052b6a3d8041a814b3111a809bd24cfbb4bd4
Ivan Hofer <ivan.hofer@outlook.com>
2021-11-09 04:32:41 +0100

    setup `typesafe-i18n`

---
+++ src/i18n/de/index.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * @typedef { import('../i18n-types').Translation } Translation
+ */
+
+/** @type { Translation } */
+const de = {
+}
+
+module.exports = de
---
+++ src/i18n/en/index.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * @typedef { import('../i18n-types').BaseTranslation } BaseTranslation
+ */
+
+/** @type { BaseTranslation } */
+const en = {
+	// TODO: your translations go here
+}
+
+module.exports = en

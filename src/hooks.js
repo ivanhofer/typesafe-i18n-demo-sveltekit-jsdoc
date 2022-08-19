4bbda0f428d7f0fdaf13845e14457c18a6f5778a
Ivan Hofer <ivan.hofer@outlook.com>
2022-08-19 12:55:07 +0200

    svelte-migrate: updated files

--- src/hooks.js
+++ src/hooks.js
@@ -1,38 +1,38 @@
 import { detectLocale } from '$i18n/i18n-util'
 import { initAcceptLanguageHeaderDetector } from 'typesafe-i18n/detectors'
 
-/** @param { string } locale */
-const htmlLanguageAttributeReplacer =
-	(locale) =>
-	({ html }) =>
-		html.replace('<html lang="en">', `<html lang="${locale}">`)
-
 /** @type { import('@sveltejs/kit').Handle } */
 export const handle = async ({ event, resolve }) => {
 	// read language slug
 	const [, lang] = event.url.pathname.split('/')
 
+	if (!lang) {
+		const locale = getPreferredLocale(event)
+
+		return new Response(null, {
+			status: 302,
+			headers: { 'Location': `/${locale}` }
+		})
+	}
+
 	// replace html lang attribute with correct language
-	return resolve(event, { transformPageChunk: htmlLanguageAttributeReplacer(lang) })
+	return resolve(event, { transformPageChunk: ({ html }) => html.replace('%lang%', lang) })
 }
 
-/** @type { import('@sveltejs/kit').GetSession } */
-export const getSession = (event) => {
+/** @type { (event: import('@sveltejs/kit').RequestEvent) => string } */
+const getPreferredLocale = (event) => {
 	// detect the preferred language the user has configured in his browser
 	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language
-	const headers = getHeaders(event)
+	const headers = transformHeaders(event)
 	const acceptLanguageDetector = initAcceptLanguageHeaderDetector({ headers })
-	const locale = detectLocale(acceptLanguageDetector)
 
-	return {
-		locale,
-	}
+	return detectLocale(acceptLanguageDetector)
 }
 
 /** @type { (event: import('@sveltejs/kit').RequestEvent) => Record<string, string> } */
-const getHeaders = (event) => {
+const transformHeaders = ({ request }) => {
 	const headers = /** @type { Record<string, string> } */ ({})
-	event.request.headers.forEach((value, key) => (headers[key] = value))
+	request.headers.forEach((value, key) => (headers[key] = value))
 
 	return headers
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,48 +1,11 @@
-<script context="module">
-	throw new Error("@migration task: Check code was safely removed (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292722)");
-
-	// import { replaceLocaleInUrl } from '../utils'
-	// import { baseLocale, locales } from '$i18n/i18n-util'
-	// import { loadLocaleAsync } from '$i18n/i18n-util.async'
-
-	// /** @type { import('@sveltejs/kit').Load } */
-	// export const load = async ({ url, session, params }) => {
-	// 	// fallback needed because of https://github.com/sveltejs/kit/issues/3647
-	// 	const lang = /** @type { import('$i18n/i18n-types').Locales } */ (params.lang || url.pathname.split('/')[1])
-
-	// 	// redirect to preferred language if user comes from page root
-	// 	if (!lang) {
-	// 		return {
-	// 			status: 302,
-	// 			redirect: `/${session.locale}`,
-	// 		}
-	// 	}
-
-	// 	// redirect to base locale if language is not present
-	// 	if (!locales.includes(lang)) {
-	// 		return {
-	// 			status: 302,
-	// 			redirect: replaceLocaleInUrl(url.pathname, baseLocale),
-	// 		}
-	// 	}
-
-	// 	// delete session locale since we don't need it to be sent to the client
-	// 	delete session.locale
-
-	// 	await loadLocaleAsync(lang)
-
-	// 	return { props: { locale: lang } }
-	// }
-</script>
-
 <script>
-	throw new Error("@migration task: Add data prop (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292707)");
-
 	import Header from '$lib/Header.svelte'
 	import { setLocale } from '$i18n/i18n-svelte'
 
-	export let locale
-	setLocale(locale)
+	/** @type { import('./$types').LayoutData } */
+	export let data
+
+	setLocale(data.locale)
 </script>
 
 <Header />
